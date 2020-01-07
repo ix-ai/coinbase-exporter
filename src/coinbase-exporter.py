01513b215d725736d9e5ff1771d9313d75fc1ec2
Alex Thomae <alex@thom.ae>
2020-01-07 06:49:26 +0100

    Adds version logging output to the running exporter

--- src/coinbase-exporter.py
+++ src/coinbase-exporter.py
@@ -9,6 +9,7 @@ import pygelf
 from prometheus_client import start_http_server
 from prometheus_client.core import REGISTRY, GaugeMetricFamily
 from coinbase.wallet.client import Client
+import constants
 
 LOG = logging.getLogger(__name__)
 logging.basicConfig(
@@ -151,7 +152,13 @@ class CoinbaseCollector:
 if __name__ == '__main__':
     configure_logging()
     PORT = int(os.environ.get('PORT', '9308'))
-    LOG.info("Starting on port {}".format(PORT))
+    LOG.warning(
+        "Starting coinbase-exporter {} with on port {}".format(
+            # pylint: disable=no-member
+            constants.VERSION,
+            PORT
+        )
+    )
     REGISTRY.register(CoinbaseCollector())
     start_http_server(PORT)
     while True:
